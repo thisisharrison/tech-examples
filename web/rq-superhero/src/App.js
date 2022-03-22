e34f8630a6ac87224339df398c79756b572b7f4a
Harrison Lau <harrison.lau@outlook.com>
2022-03-22 22:06:10 +0800

    feat: query cache

--- web/rq-superhero/src/App.js
+++ web/rq-superhero/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { QueryClientProvider, QueryClient } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import './App.css'
 import { HomePage } from './components/Home.page'
 import { RQSuperHeroesPage } from './components/RQSuperHeroes.page'
@@ -32,6 +33,7 @@ function App() {
           </Routes>
         </div>
       </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
     </QueryClientProvider>
   )
 }
--- web/rq-superhero/src/components/RQSuperHeroes.page.js
+++ web/rq-superhero/src/components/RQSuperHeroes.page.js
@@ -9,10 +9,21 @@ const fetchSuperHeroes = () => {
 // useQuery
 // 1st arg: unique key to identify query
 // 2nd arg: function returns a promise
+// 3rd arg: config
+
+// every query is cached for 5min (300000), after that it will be garbage collected
+// it is cached using the key and the fetch function as ID 
+// background refetch will occur (isFetching flag)
+// update db.json and will see it refetches in the background and update this page, without seeing isLoading
 
 /** The RQ way of doing things */
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error } = useQuery('superheroes', fetchSuperHeroes)
+  const { isLoading, data, isError, error, isFetching } = useQuery('superheroes', fetchSuperHeroes, {
+    // will be garbage collected after 5s
+    cacheTime: 5000
+  })
+
+  console.log({ isLoading, isFetching })
 
   if (isLoading) {
     return <h2>Loading...</h2>
